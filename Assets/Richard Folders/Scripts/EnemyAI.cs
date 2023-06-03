2dd7f46ef6b1f8549d2756bed53cc82524ec4d2b
Richard Brent <roxas90111@gmail.com>
2023-06-03 19:28:09 -0400

    6_3_2023 7:28 PM

--- Assets/Richard Folders/Scripts/EnemyAI.cs
+++ Assets/Richard Folders/Scripts/EnemyAI.cs
@@ -19,6 +19,23 @@ public class EnemyAI : MonoBehaviour
         state = State.Roaming;
     }
 
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
 
+    private IEnumerator RoamingRoutine()
+    {
+        while(state == State.Roaming)
+        {
+            Vector2 roamPosition = GetRoamingPosition();
+            enemyPathfinding.MoveTo(roamPosition);
+            yield return new WaitForSeconds(2f);
+        }
+    }
 
+    private Vector2 GetRoamingPosition() 
+    {
+        return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
 }
--- Assets/Richard Folders/Scripts/EnemyPathfinding.cs
+++ Assets/Richard Folders/Scripts/EnemyPathfinding.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class EnemyPathfinding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDir;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.MovePosition(rb.position + moveDir * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    public void MoveTo(Vector2 targetPosition)
+    {
+        moveDir = targetPosition;
     }
 }
