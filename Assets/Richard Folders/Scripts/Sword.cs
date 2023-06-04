d2f84c205385d8c2b3a9f486f5f0726d296692dc
Richard Brent <roxas90111@gmail.com>
2023-06-04 03:11:57 -0400

    6_4_2023 3:11 AM
    The sword now can flip between both sides of the screen.

--- Assets/Richard Folders/Scripts/Sword.cs
+++ Assets/Richard Folders/Scripts/Sword.cs
@@ -6,9 +6,13 @@ public class Sword : MonoBehaviour
 {
     private PlayerControls playerControls;
     private Animator myAnimator;
+    private PlayerController playerController;
+    private ActiveWeapon activeWeapon;
 
     private void Awake()
     {
+        playerController = GetComponentInParent<PlayerController>();
+        activeWeapon = GetComponentInParent<ActiveWeapon>();
         myAnimator = GetComponent<Animator>();
         playerControls = new PlayerControls();
     }
@@ -23,8 +27,30 @@ public class Sword : MonoBehaviour
         playerControls.Combat.Attack.started += _ => Attack();
     }
 
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
     private void Attack()
     {
         myAnimator.SetTrigger("Attack");
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(playerController.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, -180, angle);
+        }
+        else
+        {
+            activeWeapon.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
