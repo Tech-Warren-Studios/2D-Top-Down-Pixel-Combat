304b56ea155b3dcb4c90b8e502fc58c4bd931864
Richard Brent <roxas90111@gmail.com>
2023-07-09 04:18:31 -0400

    7_9_2023 4:18 AM

--- Assets/Richard Folders/Scripts/Inventory/Staff.cs
+++ Assets/Richard Folders/Scripts/Inventory/Staff.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class Staff : MonoBehaviour, Iweapon
 {
+    private void Update()
+    {
+        MouseFollowWithOffset();
+    }
+
     public void Attack()
     {
         Debug.Log("Staff Attack");
         ActiveWeapon.Instance.ToggleIsAttacking(false);
+    }
+
+    private void MouseFollowWithOffset()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(PlayerController.Instance.transform.position);
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, -180, angle);
+        }
+        else
+        {
+            ActiveWeapon.Instance.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
