2f16ddaf792a78c2a493f8c5049af617fb89a9e7
Richard Brent <roxas90111@gmail.com>
2023-07-13 16:20:21 -0400

    7_13_2023 4:20 PM
    Bow effects and colliders

--- Assets/Richard Folders/Scripts/Inventory/Projectile.cs
+++ Assets/Richard Folders/Scripts/Inventory/Projectile.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 22f;
+    [SerializeField] private GameObject particleOnHitPrefabVFX;
 
     private void Update()
     {
         MoveProjectile();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();   
+        Indestructible indestructible = other.GetComponent<Indestructible>();
+
+        if(!other.isTrigger && (enemyHealth || indestructible))
+        {
+        enemyHealth?.TakeDamage(1);
+        Instantiate(particleOnHitPrefabVFX, transform.position, transform.rotation);
+        Destroy(gameObject);
+        }
+
     }
 
     private void MoveProjectile()
