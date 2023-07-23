35a0a9238ebb2536f7f0d43974fcf3507b74bc35
Richard Brent <roxas90111@gmail.com>
2023-07-23 11:22:19 -0400

    7_23_2023 11:21 AM
    Player health, knockback, and flash script setup.

--- Assets/Richard Folders/Scripts/Player/PlayerController.cs
+++ Assets/Richard Folders/Scripts/Player/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : Singleton<PlayerController>
     private Rigidbody2D rb;
     private Animator myAnimator;
     private SpriteRenderer mySpriteRenderer;
+    private Knockback knockback;
     private float startingMoveSpeed;
 
     private bool facingLeft = false;
@@ -29,6 +30,7 @@ public class PlayerController : Singleton<PlayerController>
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();  
+        knockback = GetComponent<Knockback>();
     }
 
     private void Start()
@@ -69,6 +71,8 @@ public class PlayerController : Singleton<PlayerController>
 
     private void Move()
     {
+        if (knockback.GettingKnockedBack) { return; }
+
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
 
