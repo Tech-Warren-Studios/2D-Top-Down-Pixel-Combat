83b8f40261d779fc53d577cd42a9f6ba99ea5e3d
Richard Brent <roxas90111@gmail.com>
2023-07-23 22:58:43 -0400

    7_23_2023 10:58 PM
    Multiple different enemies and scene setup camera transitions.

--- Assets/Richard Folders/Scripts/Scene Management/CameraController.cs
+++ Assets/Richard Folders/Scripts/Scene Management/CameraController.cs
@@ -7,6 +7,10 @@ public class CameraController : Singleton<CameraController>
 {
     private CinemachineVirtualCamera cinemachineVirtualCamera;
 
+    private void Start()
+    {
+        SetPlayerCameraFollow();
+    }
 
     public void SetPlayerCameraFollow()
     {
