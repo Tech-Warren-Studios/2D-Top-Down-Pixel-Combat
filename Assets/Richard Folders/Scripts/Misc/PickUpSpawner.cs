7c9d46a4ab8be79af49f4ab9fc4e8543d1c04091
Richard Brent <roxas90111@gmail.com>
2023-08-17 16:46:51 -0400

    8_17_2023

--- Assets/Richard Folders/Scripts/Misc/PickUpSpawner.cs
+++ Assets/Richard Folders/Scripts/Misc/PickUpSpawner.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class PickUpSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject goldCoinPrefab;
+    [SerializeField] private GameObject goldCoin, healthGlobe, staminaGlobe;
 
     public void DropItems()
     {
-        Instantiate(goldCoinPrefab, transform.position, Quaternion.identity);
+        int randomNum = Random.Range(1, 5);
+
+        if (randomNum == 1)
+        {
+            Instantiate(healthGlobe, transform.position, Quaternion.identity);
+        }
+
+        if (randomNum == 2)
+        {
+            Instantiate(staminaGlobe, transform.position, Quaternion.identity);
+        }
+
+        if (randomNum == 3) 
+        {
+            int randomAmountOfGold = Random.Range(1, 4);
+
+            Instantiate(goldCoin, transform.position, Quaternion.identity);
+        }
     }
 }
