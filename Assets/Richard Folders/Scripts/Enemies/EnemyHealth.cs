a7b88ec0ee46a9d0d842f53e4355a5a52d1546af
Richard Brent <roxas90111@gmail.com>
2023-08-29 20:09:54 -0400

    8_29_2023
    Health slider and UI elements. Enemies and bushes drop items.

--- Assets/Richard Folders/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Richard Folders/Scripts/Enemies/EnemyHealth.cs
@@ -42,6 +42,7 @@ public class EnemyHealth : MonoBehaviour
         if(currentHealth <= 0)
         {
             Instantiate(deathVFXPreafab, transform.position, Quaternion.identity);
+            GetComponent<PickUpSpawner>().DropItems();
             Destroy(gameObject);
         }
     }
