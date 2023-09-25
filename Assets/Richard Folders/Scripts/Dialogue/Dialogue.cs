e5bfdb35671db25008af4c01074246f9b1488cb0
Richard Brent <roxas90111@gmail.com>
2023-09-25 12:02:34 -0400

    9_25_2023
    Dialogue Box

---
+++ Assets/Richard Folders/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    public int index;
+
+
+    private void Start()
+    {
+        textComponent.text = string.Empty;
+        startDialogue();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            } 
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];  
+            }
+        }
+    }
+
+
+    private void startDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
